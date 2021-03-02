83cb2bbdda9ca16a4124bc2b62be0ea0e17ded0d
Sergey <seriy1@gmail.com>
2021-03-02 06:44:59 -0800

    added terms & about pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article= Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: "new"
@@ -17,6 +16,17 @@ class ArticlesController < ApplicationController
   end
   def show
     @article = Article.find(params[:id])
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: "edit"
+    end
   end
   private
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
+  get '/terms' => 'pages#terms'
+  get '/about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
   resources :articles
