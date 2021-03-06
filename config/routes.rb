32c79658f67466bf27beb6dfe33d1eec26af1229
Sergey <seriy1@gmail.com>
2021-03-06 02:45:42 -0800

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
-  resources :articles
+  resources :articles do
+    resource :comments, only: [:create]
+  end
 
 end
